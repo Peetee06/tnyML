174f1da402cde5c90c3328f7de31caa464d3b682
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 17:20:33 +0200

    trying solution for junit not finding reports https://stackoverflow.com/questions/52613692/jenkins-junit-plugin-gives-error-error-no-test-report-files-were-found-config

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -2,4 +2,4 @@
 
 source ${VENV_BIN_PATH}activate
 # FIXME: for debugging
-${VENV_BIN_PATH}pytest ${WORKSPACE}/flask/server/test --junitxml=./test_xml_reports/output.xml
+${VENV_BIN_PATH}pytest ${WORKSPACE}/flask/server/test --junitxml=${WORKSPACE}/test-reports/output.xml
