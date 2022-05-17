03320fabec402f4a99121f5eb3e3ce78606af413
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 14:36:33 +0200

    back to pip install as pure setup.py didn't work

--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -5,5 +5,4 @@ python -m venv ${VENV_NAME}
 # activate venv
 source ${VENV_BIN_PATH}activate
 # install dependencies
-#${VENV_BIN_PATH}pip install -r ${WORKSPACE}/flask/server/requirements.txt --cache-dir ${WORKSPACE}/.pipcache/
-python ${WORKSPACE}/flask/server/setup.py
+${VENV_BIN_PATH}pip install ${WORKSPACE}/flask/server/ --cache-dir ${WORKSPACE}/.pipcache/
