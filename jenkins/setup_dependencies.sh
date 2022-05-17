efb91eeb3989a246cf0cbb64c1dd34e9855f84d4
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 14:15:54 +0200

    changed erronous path and removed requirements.txt now using only setup.py

--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -5,5 +5,5 @@ python -m venv ${VENV_NAME}
 # activate venv
 source ${VENV_BIN_PATH}activate
 # install dependencies
-${VENV_BIN_PATH}pip install -r ${WORKSPACE}/flask/server/requirements.txt --cache-dir ${WORKSPACE}/.pipcache/
-echo pip install successfull
+#${VENV_BIN_PATH}pip install -r ${WORKSPACE}/flask/server/requirements.txt --cache-dir ${WORKSPACE}/.pipcache/
+${VENV_BIN_PATH}python setup.py
