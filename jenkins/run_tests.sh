12774ec9d662937faa65c039cfa5163d022a8c50
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 14:47:00 +0200

    added lines for debugging

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 source ${VENV_BIN_PATH}activate
+# FIXME: for debugging
+which python
 pytest ${WORKSPACE}/flask/server/test --junitxml=./test_xml_report/output.xml
--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -4,5 +4,7 @@
 python -m venv ${VENV_NAME}
 # activate venv
 source ${VENV_BIN_PATH}activate
+# FIXME: for debugging
+which python
 # install dependencies
 ${VENV_BIN_PATH}pip install ${WORKSPACE}/flask/server/ --cache-dir ${WORKSPACE}/.pipcache/
