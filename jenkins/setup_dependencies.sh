675c7ba5744fa63c8b08d84c17c790737dc35072
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 15:08:45 +0200

    trying python install setup.py again

--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -7,4 +7,5 @@ source ${VENV_BIN_PATH}activate
 # FIXME: for debugging
 which python
 # install dependencies
-${VENV_BIN_PATH}pip install ${WORKSPACE}/flask/server/ --cache-dir ${WORKSPACE}/.pipcache/
+#${VENV_BIN_PATH}pip install ${WORKSPACE}/flask/server/ --cache-dir ${WORKSPACE}/.pipcache/
+${VENV_BIN_PATH}python install ${WORKSPACE}/flask/server/setup.py
