aeb6bc651a728f6aa68a990c223fd9d304d4135e
Peetee06 <peter_trost93@yahoo.de>
2022-05-17 16:52:04 +0200

    added collection of test reports and changed path to test reports

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -2,4 +2,4 @@
 
 source ${VENV_BIN_PATH}activate
 # FIXME: for debugging
-${VENV_BIN_PATH}pytest ${WORKSPACE}/flask/server/test --junitxml=./test_xml_report/output.xml
+${VENV_BIN_PATH}pytest ${WORKSPACE}/flask/server/test --junitxml=./test_xml_reports/output.xml
