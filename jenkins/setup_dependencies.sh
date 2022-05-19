03e242178604c660e7be8d74997e16274c5f8f2e
Peetee06 <peter_trost93@yahoo.de>
2022-05-19 16:52:45 +0200

    fixed typo in pip install command + and removed wheel install as it should not be needed anymore

--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -4,8 +4,5 @@
 python3 -m venv ${VENV_NAME}
 # activate venv
 source "${VENV_BIN_PATH}activate"
-# install wheel
-# TODO: maybe remove wheel, might be included in google cloud image
-"${VENV_BIN_PATH}pip" install wheel
 # install dependencies
-"${VENV_BIN_PATH}pip" install "${WORKSPACE}/flask/server/" -r requirements.txt
+"${VENV_BIN_PATH}pip" install -r "${WORKSPACE}/flask/server/requirements.txt"
