6bd6f1d91e80b70add72c1c2828d2ebba18abc89
Peetee06 <peter_trost93@yahoo.de>
2022-05-19 16:49:55 +0200

    changed pip install command need to use requirements.txt now as setup.py got removed

--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -5,6 +5,7 @@ python3 -m venv ${VENV_NAME}
 # activate venv
 source "${VENV_BIN_PATH}activate"
 # install wheel
+# TODO: maybe remove wheel, might be included in google cloud image
 "${VENV_BIN_PATH}pip" install wheel
 # install dependencies
-"${VENV_BIN_PATH}pip" install "${WORKSPACE}/flask/server/"
+"${VENV_BIN_PATH}pip" install "${WORKSPACE}/flask/server/" -r requirements.txt
