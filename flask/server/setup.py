c161d98ecc44c3239e4bfcbe2bf211e5f901099a
Peetee06 <peter_trost93@yahoo.de>
2022-05-19 15:52:40 +0200

    some bugfixes for new setup.py requirements.txt syntax errors fixed added " to setup_dependencies.sh to make paths with spaces work fixed root_path was pointing to setup.py not its parent directory

--- flask/server/setup.py
+++ flask/server/setup.py
@@ -2,15 +2,12 @@ import pathlib
 from setuptools import setup, find_packages
 
 
-root_path = pathlib.Path(__file__).resolve()
+root_path = pathlib.Path(__file__).parent
 requirements_path = root_path / "requirements.txt"
-install_requires = (
-    []
-)  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+install_requires = []
 if requirements_path.is_file():
     with open(requirements_path) as f:
         install_requires = f.read().splitlines()
-
 setup(
     name="tnyml",
     version="0.0.1",
--- jenkins/setup_dependencies.sh
+++ jenkins/setup_dependencies.sh
@@ -3,8 +3,8 @@
 # create venv
 python3 -m venv ${VENV_NAME}
 # activate venv
-source ${VENV_BIN_PATH}activate
+source "${VENV_BIN_PATH}activate"
 # install wheel
-${VENV_BIN_PATH}pip install wheel
+"${VENV_BIN_PATH}pip" install wheel
 # install dependencies
-${VENV_BIN_PATH}pip install ${WORKSPACE}/flask/server/ --cache-dir ${WORKSPACE}/.pipcache/
+"${VENV_BIN_PATH}pip" install "${WORKSPACE}/flask/server/"
