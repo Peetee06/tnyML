77b3a2f494bc7b013efe8b10d0d5b601268c86f8
Peetee06 <peter_trost93@yahoo.de>
2022-05-23 09:45:49 +0200

    trying another way of installing nodejs

--- tnyML/jenkins/setup_dependencies.sh
+++ tnyML/jenkins/setup_dependencies.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo snap install node --classic
-sudo find /var/lib/snapd/snaps -name "*npm*"
-sudo apt-get install -y npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
 cd ${WORKSPACE}/tnyML
 sudo npm install
 sudo npm install -g @angular/cli
