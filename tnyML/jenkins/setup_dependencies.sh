0d7e3ed145e2ee226b0d71773b7b6ffb717fcb55
Peetee06 <peter_trost93@yahoo.de>
2022-05-23 10:05:43 +0200

    removed installation of nodejs installation is manual for now but will be moved into build of vm

--- tnyML/jenkins/setup_dependencies.sh
+++ tnyML/jenkins/setup_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
 cd ${WORKSPACE}/tnyML
 sudo npm install
 sudo npm install -g @angular/cli
