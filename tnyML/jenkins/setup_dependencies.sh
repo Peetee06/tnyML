8aa4ea3e48804b81a2ac9969cb2b5e9e9594d1e4
Peetee06 <peter_trost93@yahoo.de>
2022-05-23 09:52:51 +0200

    changed nodejs version to 16.x as that is the LTS

--- tnyML/jenkins/setup_dependencies.sh
+++ tnyML/jenkins/setup_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd ${WORKSPACE}/tnyML
 sudo npm install
