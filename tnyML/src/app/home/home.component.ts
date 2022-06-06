4b7bec18c6fbdfcb0d1d21a2273a87f7f924fd3f
henni-1991 <nolte.henning@outlook.com>
2022-06-06 22:42:13 +0200

    last improvements + smaller screensize

--- tnyML/src/app/home/home.component.ts
+++ tnyML/src/app/home/home.component.ts
@@ -14,12 +14,12 @@ export class HomeComponent {
   models: string[] = [];
   selectedModelDescr: string;
   htmlelement: HTMLElement;
-  selectedFile : File = null;
-  fileuploaded : boolean = false;
-  hideWelcome : boolean = false;
-  started : boolean = false;
+  selectedFile: File = null;
+  fileuploaded: boolean = false;
+  hideWelcome: boolean = false;
+  started: boolean = false;
   imageToShow: any;
-  result : ModelInference;
+  result: ModelInference;
 
 
   constructor(public rs: RestService) {
@@ -30,8 +30,8 @@ export class HomeComponent {
     this.getModels();
   }
   scroll(element: string) {
-   this.hideWelcome = true;
-  setTimeout(()=> {this.started = true}, 1000)
+    this.hideWelcome = true;
+    setTimeout(() => { this.started = true }, 1000)
   }
 
   public getModels() {
@@ -40,46 +40,46 @@ export class HomeComponent {
       console.log(this.models[0]);
     })
   }
-  selectedModel(modelId : string){
+  selectedModel(modelId: string) {
     this.selectedModelDescr = modelId;
     console.log(modelId);
     //this.scroll("upload");
   }
 
-  onFileSelected(event:any){
+  onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
 
-  onUpload(){
+  onUpload() {
     this.imageToShow = null;
     const fd = new FormData;
     fd.append('file', this.selectedFile, this.selectedFile.name);
     this.rs.uploadFile(fd);
-    
+
   }
 
-  showImage(){
-    
-     this.rs.downloadFile(this.selectedFile.name).subscribe((img : Blob) => 
-     {
-       this.createImageFromBlob(img);
-     });
-    this.rs.startRecognition(this.selectedModelDescr,this.selectedFile.name) 
-    .subscribe((data : ModelInference) => {
-      this.result = data;
-      console.log(data)
+  showImage() {
+
+    this.rs.downloadFile(this.selectedFile.name).subscribe((img: Blob) => {
+      this.createImageFromBlob(img);
     });
+    this.rs.startRecognition(this.selectedModelDescr, this.selectedFile.name)
+      .subscribe((data: ModelInference) => {
+        this.result = data;
+        console.log(data)
+      });
 
   }
   createImageFromBlob(image: Blob) {
-    let reader = new FileReader();
-    reader.addEventListener("load", () => {
-       this.imageToShow = reader.result;
-    }, false);
- 
+
+    var reader = new FileReader();
+    reader.addEventListener('load', () => {
+      this.imageToShow = reader.result;
+    })
+
     if (image) {
-       reader.readAsDataURL(image);
+      reader.readAsDataURL(image);
     }
- }
+  }
 
 }
