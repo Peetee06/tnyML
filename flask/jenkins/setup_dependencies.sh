dac08bd659dcd6450c94ff97b4ad158f297bdd71
Peetee06 <peter_trost93@yahoo.de>
2022-06-07 00:51:16 +0200

    added pip caching and teardown of venv

--- flask/jenkins/setup_dependencies.sh
+++ flask/jenkins/setup_dependencies.sh
@@ -5,6 +5,6 @@ python3 -m venv ${VENV_NAME}
 # activate venv
 source "${VENV_BIN_PATH}activate"
 # install wheel
-"${VENV_BIN_PATH}pip" install wheel
+"${VENV_BIN_PATH}pip" install wheel --cache-dir="${WORKSPACE}/flask/server/.pipcache"
 # install dependencies
-"${VENV_BIN_PATH}pip" install -r "${WORKSPACE}/flask/server/requirements.txt"
+"${VENV_BIN_PATH}pip" install -r "${WORKSPACE}/flask/server/requirements.txt" --cache-dir="${WORKSPACE}/flask/server/.pipcache"
